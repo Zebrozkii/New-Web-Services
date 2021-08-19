b69407b4e6f3e956fdc7c6b8be495d21fd98e38d
James Zebroski <zebrozkii@gmail.com>
2021-08-18 22:34:47 -0700

    new project

---
+++ data/game-year.js
@@ -0,0 +1,61 @@
+{
+	"title": "Video Games by Year",
+	"games":[
+			  {
+				"Title": "Sonic The Hedgehog",
+				"Genre": "Platformer",
+				"Company": "Sega",
+				"Year": 1991,
+				"Rating": "E",
+				"Image": "sonic-the-hedgehog.jpg"
+			  },
+			  {
+				"Title": "Super Mario Kart",
+				"Genre": "Racing",
+				"Company": "Nintendo",
+				"Year": 1992,
+				"Rating": "E",
+				"Image": "mario-kart.jpg"
+			  },
+			  {
+				"Title": "Portal",
+				"Genre": "Puzzle",
+				"Company": "Valve",
+				"Year": 2007,
+				"Rating": "T",
+				"Image": "portal.jpg"
+			  },
+			  {
+				"Title": "Minecraft",
+				"Genre": "Sandbox",
+				"Company": "Mojang",
+				"Year": 2011,
+				"Rating": "E10",
+				"Image": "minecraft.jpg"
+			  },
+			  {
+				"Title": "Counter-Strike Global Offensive",
+				"Genre": "Tactical Shooter",
+				"Company": "Valve",
+				"Year": 2012,
+				"Rating": "M",
+				"Image": "csgo.jpg"
+			  }
+
+			]
+}
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
